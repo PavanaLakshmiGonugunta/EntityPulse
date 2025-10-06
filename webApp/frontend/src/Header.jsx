16bcc5cf2282d3779755bb6dd77b3fb83fb0237c
Sravani <sravanijanak@gmail.com>
2025-10-06 13:56:58 +0530

    Resolved merge conflicts

--- webApp/frontend/src/Header.jsx
+++ webApp/frontend/src/Header.jsx
@@ -4,15 +4,16 @@ import { CiSearch } from "react-icons/ci";
 import { FcAbout } from "react-icons/fc";
 import { BsFileBarGraph } from "react-icons/bs";
 import About from "./About";
+import { Link } from 'react-router-dom';
+
 const Header = ()=>{
     return(
         <>
             <div className="head">
             <header  className="title"><i><BsFileBarGraph /></i> <strong>Sentiment Scope</strong></header>
-                <a><IoMdHome />Analyse</a>
-                <a><CiSearch />Search</a>
-                <a><FcAbout/>About</a>
-                
+               <Link to={"/"} className="nav"><i><IoMdHome size={20}/></i>Home</Link>
+               <Link to={"/search"} className="nav"><i><CiSearch size={20}/></i>Search</Link>
+               <Link to={"/about"} className="nav"><i><FcAbout size={20}/></i>About</Link>
             </div>
             
         </>
